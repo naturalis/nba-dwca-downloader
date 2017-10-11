6f0015152c3b1decd735a024a027b12ec7dbc443
maarten schermer <maarten.schermer@naturalis.nl>
2017-10-11 16:09:33 +0200

    first commit

---
+++ data_set_names.py
@@ -0,0 +1,4 @@
+import json, sys
+obj=json.load(sys.stdin)
+for x in obj:
+    print(x)
---
+++ dwca-download.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+date
+
+SERVER_ADDR_FILE=/var/www/drupal/sites/default/files/nbabase.txt
+
+if [ ! -f $SERVER_ADDR_FILE ]; then
+  echo "$SERVER_ADDR_FILE not found"
+  exit
+fi
+
+SERVER=$(cat $SERVER_ADDR_FILE | sed -e 's/\/v2//')
+OUTDIR=downloads
+
+datestamp() {
+  date +"%Y%m%d"
+}
+
+OUT=$(curl -s -XGET $SERVER/v2/specimen/dwca/getDataSetNames)
+NAMES=$(echo  $OUT | python3 data_set_names.py)
+
+for i in $NAMES; do
+        echo "downloading $i"
+        curl -o ${OUTDIR}/tmp-${i}-`datestamp`.dwca.zip.part $SERVER/v2/specimen/dwca/getDataSet/$i
+        echo "deleting previous ${i} download"
+        rm -f ${OUTDIR}/${i}*
+        mv ${OUTDIR}/tmp-${i}-`datestamp`.dwca.zip.part ${OUTDIR}/${i}-`datestamp`.dwca.zip
+done
+
+
+OUT=$(curl -s -XGET $SERVER/v2/taxon/dwca/getDataSetNames)
+NAMES=$(echo  $OUT | python3 data_set_names.py)
+
+for i in $NAMES; do
+        echo "downloading $i"
+        curl -o ${OUTDIR}/tmp-${i}-`datestamp`.dwca.zip.part $SERVER/v2/taxon/dwca/getDataSet/$i
+        echo "deleting previous ${i} download"
+        rm -f ${OUTDIR}/${i}*
+        mv ${OUTDIR}/tmp-${i}-`datestamp`.dwca.zip.part ${OUTDIR}/${i}-`datestamp`.dwca.zip
+done
+
+date
+
+echo "done"
+
